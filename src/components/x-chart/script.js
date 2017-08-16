1673c8f73bbff9859a8cd66152717c383f52459a
Anastasiia <kozenko.anastasiia@gmail.com>
2017-08-16 21:03:35 +0300

    Change X-Chart

--- src/components/x-chart/script.js
+++ src/components/x-chart/script.js
@@ -8,57 +8,25 @@
       this.attachShadow({ mode: 'open' });
 
       this.chart = componentTemplate.querySelector('.chart');
-      this.draw();
-      this.onPageResize = this.onPageResize.bind(this);
-      window.addEventListener('resize', this.onPageResize);
+      this.svg = this.chart.querySelector('svg');
+
+      this.chart.querySelector('h3').innerHTML = this.innerHTML;
+
+      const percent = this.getAttribute('percent');
+      this.svg.querySelector('text').innerHTML = percent;
+
+      const circleList = this.svg.querySelectorAll('circle');
+      const strokeWidth = this.getAttribute('stroke-width');
+
+      if (strokeWidth) {
+        Array.from(circleList).forEach(circle => circle.setAttribute('stroke-width', strokeWidth));
+      }
+
+      const circumference = 465; // PI * 2R
+      const circleStrokeDasharray = parseFloat(percent) * circumference / 100;
+      circleList[1].setAttribute('stroke-dasharray', circleStrokeDasharray);
 
       this.shadowRoot.appendChild(componentTemplate.cloneNode(true));
-    }
-
-    draw() {
-      this.drawChart();
-      this.chart.querySelector('h3').innerHTML = this.innerHTML;
-      this.chart.querySelector('p').innerHTML = this.getAttribute('percent');
-    }
-
-    drawChart() {
-      this.svg = this.chart.querySelector('svg');
-      this.setParams();
-      this.setSvgAttributes();
-
-      const circle = this.chart.querySelector('.circle');
-      const circleValue = parseFloat(this.getAttribute('percent'));
-      const circleStrokeDasharray = circleValue * this.circumference / 100;
-      this.svg.innerHTML = this.drawCircle() + this.drawCircle(circleStrokeDasharray);
-    }
-
-    setParams() {
-      this.svgSize = this.getBoundingClientRect().width;
-      this.strokeWidth = 10;
-      this.circleRadius = this.svgSize / 2 - this.strokeWidth * 2;
-      this.circleCenterX = -this.svgSize / 2;
-      this.circleCenterY = this.svgSize / 2;
-      this.circumference = Math.PI * this.circleRadius * 2;
-    }
-
-    setSvgAttributes() {
-      this.svg.setAttribute('width', this.svgSize);
-      this.svg.setAttribute('height', this.svgSize);
-      this.svg.setAttribute('viewBox', `0 0 ${this.svgSize} ${this.svgSize}`);
-    }
-
-    drawCircle(strokeDasharray = this.circumference) {
-      return ('<circle ' +
-        'stroke-width="' + this.strokeWidth + '" ' +
-        'stroke-dasharray="' + strokeDasharray + '" ' +
-        'r="' + this.circleRadius + '" ' +
-        'cx="' + this.circleCenterX + '" ' +
-        'cy="' + this.circleCenterY + '"' +
-      '/>');
-    }
-
-    onPageResize() {
-      this.drawChart();
     }
   };
 
