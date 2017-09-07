025bf0026285d57d8a5f2b03cc8779001cad623f
Anastasiia <kozenko.anastasiia@gmail.com>
2017-09-07 15:42:58 +0300

    Bugfix

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,59 @@
-const CACHE_NAME = 'static';
+const CACHE_NAME = 'blue-shark';
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.min.css',
-  '/script.min.js',
-  '/components/',
-  '/fonts/',
-  '/icons/',
-  '/img/',
-  '/sprite.png',
+  './',
+  './index.html',
+  './style.min.css',
+  './script.min.js',
+  './sprite.png',
+  './components/x-navigation/',
+  './components/x-navigation/index.html',
+  './components/x-navigation/style.css',
+  './components/x-navigation/script.js',
+  './components/x-navigation-item/',
+  './components/x-navigation-item/index.html',
+  './components/x-navigation-item/style.css',
+  './components/x-navigation-item/script.js',
+  './components/x-chart/',
+  './components/x-chart/index.html',
+  './components/x-chart/style.css',
+  './components/x-chart/script.js',
 ];
 
 this.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Save cache');
+      console.log('Cache opened');
       return cache.addAll(urlsToCache);
     })
   );
 });
 
 this.addEventListener('fetch', event => {
-  let response;
   event.respondWith(
-    caches
-      .match(event.request)
-      .catch(() => fetch(event.request))
-      .then(r => {
-        console.log('Load data from server');
-        response = r;
-        caches.open(CACHE_NAME).then(cache => {
-          console.log('Update cache');
-          cache.put(event.request, response);
-        });
-        return response.clone();
-      })
-      .catch(() => caches.match('/img/'))
+    caches.match(event.request).then(response => {
+      if (response) {
+        console.log(`Load from cache: ${response.url}`);
+        return response;
+      }
+      console.log(`Load from server: ${event.request.url}`);
+      return fetch(event.request);
+    })
   );
+  // let response;
+  // event.respondWith(
+  //   caches
+  //     .match(event.request)
+  //     .catch(() => fetch(event.request))
+  //     .then(r => {
+  //       console.log('Load data from server');
+  //       response = r;
+  //       caches.open(CACHE_NAME).then(cache => {
+  //         console.log('Update cache');
+  //         cache.put(event.request, response);
+  //       });
+  //       return response.clone();
+  //     })
+  //     .catch(() => console.log('ERROR!!!!'))
+  // );
 });
--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/service-worker.js')
+    .register('./service-worker.js')
     .then(reg => {
       console.info(`Registration succeeded. Scope is ${reg.scope}`);
     })
