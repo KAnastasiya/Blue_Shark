7c3fe542aecb2ea00e294aaa3e7fd1c6f1bf741c
Anastasiia <kozenko.anastasiia@gmail.com>
2017-09-07 17:16:44 +0300

    Bugfix
    Bugfix

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,15 @@
-const CACHE_NAME = 'blue-shark';
+const CACHE_NAME = 'blue-shark-v1';
 
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.min.css',
-  './script.min.js',
-  './sprite.png',
-  './components/x-navigation/',
-  './components/x-navigation/index.html',
-  './components/x-navigation/style.css',
-  './components/x-navigation/script.js',
-  './components/x-navigation-item/',
-  './components/x-navigation-item/index.html',
-  './components/x-navigation-item/style.css',
-  './components/x-navigation-item/script.js',
-  './components/x-chart/',
-  './components/x-chart/index.html',
-  './components/x-chart/style.css',
-  './components/x-chart/script.js',
-];
+const urlsToCache = ['./', './index.html', './style.min.css', './script.min.js', './sprite.png'];
 
 this.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Cache opened');
+      console.log(`Create cache ${CACHE_NAME}`);
       return cache.addAll(urlsToCache);
     })
   );
 });
-
-// this.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       if (response) {
-//         console.log(`Load from cache: ${response.url}`);
-//         return response;
-//       }
-//       console.log(`Load from server: ${event.request.url}`);
-//       return fetch(event.request);
-//     })
-//   );
-// });
 
 this.addEventListener('fetch', event => {
   event.respondWith(
@@ -65,21 +34,18 @@ this.addEventListener('fetch', event => {
   );
 });
 
-// this.addEventListener('fetch', event => {
-//   let response;
-//   event.respondWith(
-//     caches
-//       .match(event.request)
-//       .catch(() => fetch(event.request))
-//       .then(r => {
-//         response = r;
-//         console.log(`Load from server: ${response.url}`);
-//         caches.open(CACHE_NAME).then(cache => {
-//           console.log(`Load in cache: ${response.url}`);
-//           cache.put(event.request, response);
-//         });
-//         return response.clone();
-//       })
-//       .catch(() => caches.match('./img/portfolio-default.jpg'))
-//   );
-// });
+this.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
