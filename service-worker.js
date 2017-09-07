b81fe36bf3a57900dd3df94ec18c8af3457f2b00
Anastasiia <kozenko.anastasiia@gmail.com>
2017-09-07 15:51:21 +0300

    Update data in cache

--- service-worker.js
+++ service-worker.js
@@ -29,6 +29,19 @@ this.addEventListener('install', event => {
   );
 });
 
+// this.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request).then(response => {
+//       if (response) {
+//         console.log(`Load from cache: ${response.url}`);
+//         return response;
+//       }
+//       console.log(`Load from server: ${event.request.url}`);
+//       return fetch(event.request);
+//     })
+//   );
+// });
+
 this.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
@@ -36,24 +49,37 @@ this.addEventListener('fetch', event => {
         console.log(`Load from cache: ${response.url}`);
         return response;
       }
-      console.log(`Load from server: ${event.request.url}`);
-      return fetch(event.request);
+
+      const fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(r => {
+        if (!r || r.status !== 200 || r.type !== 'basic') {
+          return r;
+        }
+
+        console.log(`Load from server: ${r.url}`);
+        const responseToCache = r.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache));
+        return r;
+      });
     })
   );
-  // let response;
-  // event.respondWith(
-  //   caches
-  //     .match(event.request)
-  //     .catch(() => fetch(event.request))
-  //     .then(r => {
-  //       console.log('Load data from server');
-  //       response = r;
-  //       caches.open(CACHE_NAME).then(cache => {
-  //         console.log('Update cache');
-  //         cache.put(event.request, response);
-  //       });
-  //       return response.clone();
-  //     })
-  //     .catch(() => console.log('ERROR!!!!'))
-  // );
 });
+
+// this.addEventListener('fetch', event => {
+//   let response;
+//   event.respondWith(
+//     caches
+//       .match(event.request)
+//       .catch(() => fetch(event.request))
+//       .then(r => {
+//         response = r;
+//         console.log(`Load from server: ${response.url}`);
+//         caches.open(CACHE_NAME).then(cache => {
+//           console.log(`Load in cache: ${response.url}`);
+//           cache.put(event.request, response);
+//         });
+//         return response.clone();
+//       })
+//       .catch(() => caches.match('./img/portfolio-default.jpg'))
+//   );
+// });
