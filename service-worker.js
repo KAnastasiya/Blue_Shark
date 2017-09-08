5b8337a9b742456036111aff680732d4296a12e0
Anastasiia <kozenko.anastasiia@gmail.com>
2017-09-08 12:31:32 +0300

    Change service-worker.js
    Add loading data from server and updating cache

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,75 @@
-const CACHE_NAME = 'blue-shark-v2';
+const CACHE = 'blue-shark-v3';
 
-const urlsToCache = ['./', './index.html', './style.min.css', './script.min.js', './sprite.png'];
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.min.css',
+  './script.min.js',
+  './sprite.png',
+  './components/x-navigation/index.html',
+  './components/x-navigation/script.js',
+  './components/x-navigation/style.css',
+  './components/x-navigation-item/index.html',
+  './components/x-navigation-item/script.js',
+  './components/x-navigation-item/style.css',
+  './components/x-chart/index.html',
+  './components/x-chart/script.js',
+  './components/x-chart/style.css',
+];
 
 this.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-this.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        return response;
-      }
-
-      const fetchRequest = event.request.clone();
-      return fetch(fetchRequest).then(r => {
-        if (!r || r.status !== 200 || r.type !== 'basic') {
-          return r;
-        }
-
-        const responseToCache = r.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache));
-        return r;
-      });
-    })
-  );
+  console.log('The service worker is being installed');
+  event.waitUntil(preCache());
 });
 
 this.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
+  console.log('The service worker is being activate');
+  event.waitUntil(cleanCache());
   return this.clients.claim();
 });
+
+this.addEventListener('fetch', event => {
+  console.log('The service worker is serving the resourses');
+  event.respondWith(
+    caches.open(CACHE).then(cache =>
+      cache.match(event.request).then(response => {
+        if (response) {
+          return response;
+        }
+        return loadDataFromServer(event.request);
+      })
+    )
+  );
+});
+
+function preCache() {
+  caches.open(CACHE).then(cache => cache.addAll(urlsToCache));
+}
+
+function updateCache(request, response) {
+  const responseToCache = response.clone();
+  caches.open(CACHE).then(c => c.put(request, responseToCache));
+}
+
+function cleanCache() {
+  caches.keys().then(keys =>
+    Promise.all(
+      keys.map(key => {
+        if (key !== CACHE) {
+          return caches.delete(key);
+        }
+      })
+    )
+  );
+}
+
+function loadDataFromServer(request) {
+  const fetchRequest = request.clone();
+  return fetch(fetchRequest).then(r => {
+    if (!r || r.status !== 200 || r.type !== 'basic') {
+      return r;
+    }
+    updateCache(request, r);
+    return r;
+  });
+}
